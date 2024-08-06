3ba222822fc1b9906d73785bb30750dfcc56c63a
Stone Tao <stonezt2019@gmail.com>
2024-08-06 14:12:53 -0700

    Update ground.py

--- mani_skill/utils/building/ground.py
+++ mani_skill/utils/building/ground.py
@@ -3,7 +3,6 @@ Useful utilities for creating the ground of a scene
 """
 from __future__ import annotations
 
-import os.path as osp
 from typing import TYPE_CHECKING
 
 import numpy as np
@@ -31,70 +30,75 @@ def build_ground(
     ground.add_plane_collision(
         sapien.Pose(p=[0, 0, altitude], q=[0.7071068, 0, -0.7071068, 0]),
     )
+    ground.add_plane_visual(
+        sapien.Pose(p=[0, 0, altitude], q=[0.7071068, 0, -0.7071068, 0]),
+        scale=[0.1, 100, 100],
+        material=sapien.render.RenderMaterial(base_color=[1, 1, 1, 1]),
+    )
     if scene.parallel_in_single_scene:
         # when building a ground and using a parallel render in the GUI, we want to only build one ground visual+collision plane
         ground.set_scene_idxs([0])
     actor = ground.build_static(name=name)
 
-    # generate a grid of right triangles that form 1x1 meter squares centered at (0, 0, 0)
-    floor_length = floor_width if floor_length is None else floor_length
-    num_verts = (floor_width + 1) * (floor_length + 1)
-    vertices = np.zeros((num_verts, 3))
-    floor_half_width = floor_width / 2
-    floor_half_length = floor_length / 2
-    xrange = np.arange(start=-floor_half_width, stop=floor_half_width + 1)
-    yrange = np.arange(start=-floor_half_length, stop=floor_half_length + 1)
-    xx, yy = np.meshgrid(xrange, yrange)
-    xys = np.stack((yy, xx), axis=2).reshape(-1, 2)
-    vertices[:, 0] = xys[:, 0] + xy_origin[0]
-    vertices[:, 1] = xys[:, 1] + xy_origin[1]
-    vertices[:, 2] = altitude
-    normals = np.zeros((len(vertices), 3))
-    normals[:, 2] = 1
+    # # generate a grid of right triangles that form 1x1 meter squares centered at (0, 0, 0)
+    # floor_length = floor_width if floor_length is None else floor_length
+    # num_verts = (floor_width + 1) * (floor_length + 1)
+    # vertices = np.zeros((num_verts, 3))
+    # floor_half_width = floor_width / 2
+    # floor_half_length = floor_length / 2
+    # xrange = np.arange(start=-floor_half_width, stop=floor_half_width + 1)
+    # yrange = np.arange(start=-floor_half_length, stop=floor_half_length + 1)
+    # xx, yy = np.meshgrid(xrange, yrange)
+    # xys = np.stack((yy, xx), axis=2).reshape(-1, 2)
+    # vertices[:, 0] = xys[:, 0] + xy_origin[0]
+    # vertices[:, 1] = xys[:, 1] + xy_origin[1]
+    # vertices[:, 2] = altitude
+    # normals = np.zeros((len(vertices), 3))
+    # normals[:, 2] = 1
 
-    mat = sapien.render.RenderMaterial()
-    mat.base_color_texture = sapien.render.RenderTexture2D(
-        filename=osp.join(osp.dirname(__file__), "assets/grid_texture.png"),
-    )
-    mat_square_len = 4  # hardcoded for the floor tile picture, saying that square tile is 4 meters wide
-    uv_scale = floor_width / mat_square_len
-    uvs = np.zeros((len(vertices), 2))
-    uvs[:, 0] = (xys[:, 0] * uv_scale + floor_half_width) / floor_width
-    uvs[:, 1] = (xys[:, 1] * uv_scale + floor_half_width) / floor_width
+    # mat = sapien.render.RenderMaterial()
+    # mat.base_color_texture = sapien.render.RenderTexture2D(
+    #     filename=osp.join(osp.dirname(__file__), "assets/grid_texture.png"),
+    # )
+    # mat_square_len = 4  # hardcoded for the floor tile picture, saying that square tile is 4 meters wide
+    # uv_scale = floor_width / mat_square_len
+    # uvs = np.zeros((len(vertices), 2))
+    # uvs[:, 0] = (xys[:, 0] * uv_scale + floor_half_width) / floor_width
+    # uvs[:, 1] = (xys[:, 1] * uv_scale + floor_half_width) / floor_width
 
-    # TODO: This is fast but still two for loops which is a little annoying
-    triangles = []
-    for i in range(floor_length):
-        triangles.append(
-            np.stack(
-                [
-                    np.arange(floor_width) + i * (floor_width + 1),
-                    np.arange(floor_width) + 1 + floor_width + i * (floor_width + 1),
-                    np.arange(floor_width) + 1 + i * (floor_width + 1),
-                ],
-                axis=1,
-            )
-        )
-    for i in range(floor_length):
-        triangles.append(
-            np.stack(
-                [
-                    np.arange(floor_width) + 1 + floor_width + i * (floor_width + 1),
-                    np.arange(floor_width) + floor_width + 2 + i * (floor_width + 1),
-                    np.arange(floor_width) + 1 + i * (floor_width + 1),
-                ],
-                axis=1,
-            )
-        )
-    triangles = np.concatenate(triangles)
+    # # TODO: This is fast but still two for loops which is a little annoying
+    # triangles = []
+    # for i in range(floor_length):
+    #     triangles.append(
+    #         np.stack(
+    #             [
+    #                 np.arange(floor_width) + i * (floor_width + 1),
+    #                 np.arange(floor_width) + 1 + floor_width + i * (floor_width + 1),
+    #                 np.arange(floor_width) + 1 + i * (floor_width + 1),
+    #             ],
+    #             axis=1,
+    #         )
+    #     )
+    # for i in range(floor_length):
+    #     triangles.append(
+    #         np.stack(
+    #             [
+    #                 np.arange(floor_width) + 1 + floor_width + i * (floor_width + 1),
+    #                 np.arange(floor_width) + floor_width + 2 + i * (floor_width + 1),
+    #                 np.arange(floor_width) + 1 + i * (floor_width + 1),
+    #             ],
+    #             axis=1,
+    #         )
+    #     )
+    # triangles = np.concatenate(triangles)
 
-    shape = sapien.render.RenderShapeTriangleMesh(
-        vertices=vertices, triangles=triangles, normals=normals, uvs=uvs, material=mat
-    )
+    # shape = sapien.render.RenderShapeTriangleMesh(
+    #     vertices=vertices, triangles=triangles, normals=normals, uvs=uvs, material=mat
+    # )
 
-    for obj in actor._objs:
-        floor_comp = sapien.render.RenderBodyComponent()
-        floor_comp.attach(shape)
-        obj.add_component(floor_comp)
+    # for obj in actor._objs:
+    #     floor_comp = sapien.render.RenderBodyComponent()
+    #     floor_comp.attach(shape)
+    #     obj.add_component(floor_comp)
 
     return actor
